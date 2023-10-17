6d4402308fa0c30a677f5e0c50b72defec2472ab
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2023-10-18 04:07:50 +0600

    Create Supermarket-Billing-System-Project .cpp

---
+++ Supermarket-Billing-System-Project .cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespae std;
+class shopping
+{
+
+
+private:
+  int pcd;
+  float rice;
+  float dis;
+  string pname;
+
+public:
+  void menu ();
+  void administration ();
+  void buyer ();
+  void add ();
+  void edit ();
+  void rem ();
+  void list ();
+  void receipt ();
+
+
+};
+void
+shopping::menu ()
+{
+
+  int choice;
+  string email;
+  string passward;
+
+  cout << "\t\t\t\t________________________________________\n";
+  cout << "\t\t\t\t                                         \n";
+  cout << "\t\t\t\t        SUPERMARKT MAIN MANU             \n";                   \n";
+  cout << "\t\t\t\t                                         \n";
+  cout << "\t\t\t\t_________________________________________\n";
+  cout << "\t\t\t\t                                         \n";
+  cout << "\t\t\t\t|    1) Administrator        |\n;                                 \n";
+  cout << "\t\t\t\t|                             \n; 
+  cout << "\t\t\t\t|    2)     Buyer            |\n; 
+  cout << "\t\t\t\t|                             \n; 
+  cout << "\t\t\t\t|    3)     Exit             |\n;
+
+
+    //its not finished.....but i will do it 
+    
+
+
+
+}
